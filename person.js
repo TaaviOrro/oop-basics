f1e385bad5d6d84c88c146d0952b62992e88c55b
Taavi <taavi.orro@voco.ee>
2024-12-05 13:50:59 +0200

    basic

---
+++ person.js
@@ -0,0 +1,18 @@
+class Person {
+    constructor(name){
+        this.name = name
+    }
+    setdateofBirth(year){
+        this.birthyear = birthyear
+    }
+
+    getDateofBirth(){
+        
+    }
+
+    age(){
+        this.age = age
+    }
+
+
+}
